bdead626ecf96d2d186ed0070c80ded2928c6fb1
shofwanh <shofwanhanif22@gmail.com>
2020-06-19 19:10:43 +0700

    Setup Project

---
+++ src/containers/App/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './index.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="layout-main">
+                Pretest Frontend
+            </div>
+        )
+    }
+}
+
+export default App;
